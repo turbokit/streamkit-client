fbf203c61a7f396fd8917016d7dbcf5ba317cd0a
embarq <embarq.dev@gmail.com>
2017-01-16 03:41:32 +0200

    Change status-bar settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 
-
 @Component({
   templateUrl: 'app.html'
 })
@@ -12,11 +11,11 @@ export class MyApp {
   rootPage = HomePage;
 
   constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
+    platform
+      .ready()
+      .then(() => {
+        StatusBar.backgroundColorByHexString('#387ef5');
+        Splashscreen.hide();
+      });
   }
 }
