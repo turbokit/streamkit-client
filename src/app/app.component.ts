0c51a776e5459811f636878e259451c27ec38141
embarq <embarq.dev@gmail.com>
2017-01-18 03:14:43 +0200

    Implement socket.io connection and handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import * as io from 'socket.io-client';
 
 import { HomePage } from '../pages/home/home';
 
@@ -14,6 +15,10 @@ export class MyApp {
     platform
       .ready()
       .then(() => {
+        io('http://localhost:5000/test')
+          .open()
+          .on('my_response', data => console.log(data));
+
         StatusBar.backgroundColorByHexString('#387ef5');
         Splashscreen.hide();
       });
