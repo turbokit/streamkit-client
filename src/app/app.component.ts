93f2f9fff193e25ef51f90a964b50b5e70470194
embarq <embarq.dev@gmail.com>
2017-02-25 17:25:04 +0200

    Remove unnecessary socket.io refs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import * as io from 'socket.io-client';
 
 import { HomePage } from '../pages/home/home';
 
@@ -15,10 +14,6 @@ export class MyApp {
     platform
       .ready()
       .then(() => {
-        io('http://localhost:5000/test')
-          .open()
-          .on('my_response', data => console.log(data));
-
         StatusBar.backgroundColorByHexString('#387ef5');
         Splashscreen.hide();
       });
